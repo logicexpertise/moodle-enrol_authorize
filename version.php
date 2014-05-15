0289d0f0ed507dd3f80e56f370b8ab31752f886f
Olumuyiwa Taiwo <muyi.taiwo@logicexpertise.com>
2014-05-16 02:54:10 +1000

    Added missing includes in enrol_form.php

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2014051600;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = 2014051601;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2013050100;        // Requires this Moodle version
 $plugin->component = 'enrol_authorize'; // Full name of the plugin (used for diagnostics)
 $plugin->cron      = 180;
